5ed69392f5474ec4fe13e4bab97d64a3f58ca493
klltx <kostya.svetashov.01@gmail.com>
2021-03-02 14:35:14 +0400

    feat: lesson02

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 100;
+let income = 'Freelance';
+let addExpenses = 'Taxi, food, room, life';
+let deposit = false;
+let mission = 1000;
+let period = 1;
+let budgetDay = Math.floor(mission / 30);
 
-alert('hello');
-console.log('hello');
+// console.log(typeof money + ' ' + typeof income + ' ' + typeof deposit);
+// console.log(addExpenses.length);
+// console.log('Период равен ' + period + ' месяцев');
+// console.log('Цель заработать ' + mission + ' рублей');
+
+// console.log(addExpenses.toLowerCase().split(', '));
+// console.log(budgetDay);
+
+// let num = 266219;
+// let res = 1;
+// let arr = String(num);
+
+// for(let i = 0; i < arr.length; i++){
+//     res *= Number(arr[i]);
+// }
+// console.log(res);
+// res = res ** 3;
+
+// console.log(res);
+// console.log(String(res).substr(0, 2));
